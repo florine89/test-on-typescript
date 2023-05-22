c905e171ea1a89ccb2338b60c10b44a1c50dfa10
florine89 <114070434+florine89@users.noreply.github.com>
2023-05-22 15:19:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const user = {
+    name: 'flo',
+    age:'30',
+    address: {
+        city: 'New York',
+        state: 'NY'
+    },
+}
+
+console.log(user.address.city);
+console.log(user.toto);
